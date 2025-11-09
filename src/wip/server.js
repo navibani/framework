0d8b9208903641a5c09e5e838eb74280ff4b41ba
Navraj Bains <navraj.bains.89@outlook.com>
2025-11-09 14:50:49 -0500

    Basic server sending text

--- src/wip/server.js
+++ src/wip/server.js
@@ -1,11 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
-const server = (0, http_1.createServer)((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello, World!\n");
-});
 const PORT = 3000;
+const server = (0, http_1.createServer)((req, res) => {
+    try {
+        const { url, method, headers } = req;
+        if (url === undefined || method === undefined) {
+            throw new Error('Invalid request');
+        }
+        const parsedUrl = new URL(url, `http://${headers.host}`);
+        const pathname = parsedUrl.pathname || '/';
+        console.log('Request Path:', pathname);
+        console.group(url + ' ' + method);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('Hello, World!\n');
+    }
+    catch (error) { }
+});
 server.listen(PORT, 'localhost', () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
--- src/wip/server.ts
+++ src/wip/server.ts
@@ -1,11 +1,26 @@
-import { createServer } from "http";
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
-});
+import { createServer } from 'http';
+import { parse } from 'url';
 
 const PORT = 3000;
+
+const server = createServer((req, res) => {
+  try {
+    const { url, method, headers } = req;
+
+    if (url === undefined || method === undefined) {
+      throw new Error('Invalid request');
+    }
+
+    const parsedUrl = new URL(url, `http://${headers.host}`);
+    const pathname = parsedUrl.pathname || '/';
+
+    console.log('Request Path:', pathname);
+
+    console.group(url + ' ' + method);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, World!\n');
+  } catch (error) {}
+});
 
 server.listen(PORT, 'localhost', () => {
   console.log(`Server running at http://localhost:${PORT}/`);
