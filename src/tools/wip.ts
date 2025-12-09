2336e89d5475373253aabf143bb78d7131122355
Navraj Bains <navraj.bains.89@outlook.com>
2025-12-09 17:45:12 -0500

    Rough error handling function.

--- src/tools/wip.ts
+++ src/tools/wip.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 function handleErrors(exportFunctions: { [key: string]: (args?: any) => any }) {
   const keyPair = Object.entries(exportFunctions);
 
@@ -8,22 +10,43 @@ function handleErrors(exportFunctions: { [key: string]: (args?: any) => any }) {
         try {
           return await fn(args);
         } catch (error) {
-          console.error(`Error in function ${key}:`, error);
-          throw error;
+          const isError = error instanceof Error;
+          const name = isError ? error.name : 'UnknownError';
+          const message = isError ? error.message : 'An unknown error occurred';
+          const stack =
+            isError && error.stack ? error.stack : 'No stack trace available';
+          const string = isError ? error.toString() : String(error);
+          const date = new Date().toISOString();
+
+          const content = `Name: ${name}\nMessage: ${message}\nStack: ${stack}\nString: ${string}\nDate: ${date}\n\n`;
+
+          const directoryPath = `./dump/errors/`;
+
+          const filePath = `${directoryPath}error_${Date.now()}.log`;
+
+          fs.mkdir(directoryPath, { recursive: true }, (dirError) => {
+            if (error) console.error('Error creating directory:', dirError);
+          });
+
+          fs.writeFile(filePath, content, (error) => {
+            if (error) console.error('Error writing to file:', error);
+          });
         }
       },
     };
-  });
+  }, {} as { [key: string]: (args?: any) => Promise<any> });
 
   return reducedFn;
 }
 
-export default async function app() {
+export default function app() {
   const exportFunctions = {
     test: () => {
-      console.log('Test function executed');
+      throw new Error('This is a test error');
     },
   };
 
   return handleErrors(exportFunctions);
 }
+
+app().test();
