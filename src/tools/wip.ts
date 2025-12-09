3437e2395bca7ab95193fd400abf2ce2f3c84d58
Navraj Bains <navraj.bains.89@outlook.com>
2025-12-08 20:00:42 -0500

    Basic error handler.

--- src/tools/wip.ts
+++ src/tools/wip.ts
@@ -1,3 +1,18 @@
+async function handleError<Args, Res>({
+  process,
+  args,
+}: {
+  process: (args: Args) => Promise<Res>;
+  args: Args;
+}): Promise<Res | void> {
+  try {
+    return await process(args);
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return;
+  }
+}
+
 export default async function app() {
   return {};
 }
