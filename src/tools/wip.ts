ef654b3027cda3c9252570d8833c263f13d6938c
Navraj Bains <navraj.bains.89@outlook.com>
2025-12-19 01:08:44 -0500

    Decoupled state functions.

--- src/tools/wip.ts
+++ src/tools/wip.ts
@@ -1,19 +1,21 @@
-function createState<state>(data: state) {
-  let state: state = data;
+function getState<State>(state: State) {
+  return state;
+}
+
+function setState<State>(state: State, data: State) {
+  state = data;
+}
+
+function createState<State>(data: State) {
+  let state: State = data;
 
   return {
-    getState: () => {
-      return state;
-    },
-    setState: (data: state) => {
-      state = data;
-    },
+    getState: () => getState(state),
+    setState: (data: State) => setState(state, data),
   };
 }
 
 function createErrorHandler() {
-  const state = createState<Error | null>(null);
-
   return {};
 }
 
