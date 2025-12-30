3022f7218960ab61bbbc2ef609eb44fb5398b308
Navraj Bains <navraj.bains.89@outlook.com>
2025-12-30 17:14:53 -0500

    Basic error handler added.

--- src/tools/wip.ts
+++ src/tools/wip.ts
@@ -1,3 +1,36 @@
-function createActions() {
-  return {};
+function createApp() {
+  const actions = {
+    test1: () => {
+      console.log('basic test');
+    },
+    test2: (args: string) => {
+      console.log(args);
+      return args;
+    },
+    test3: (args: {}) => {
+      throw new Error('Test');
+    },
+  };
+
+  const wrappedActions = Object.entries(actions).reduce(
+    (actions, [key, action]) => {
+      return {
+        ...actions,
+        [key]: async (args: any) => {
+          try {
+            return await action(args);
+          } catch (error) {
+            console.log(error);
+          }
+        },
+      };
+    },
+    {} as typeof actions
+  );
+
+  return { ...wrappedActions };
 }
+
+const testApp = createApp();
+
+testApp.test3({});
