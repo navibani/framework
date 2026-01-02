7030656442e955c340aa7bd3c8473d26625772f6
Navraj Bains <navraj.bains.89@outlook.com>
2026-01-02 17:43:07 -0500

    Restart application due to failure to understand typescript mapping.

--- src/tools/wip.ts
+++ src/tools/wip.ts
@@ -1,9 +1,26 @@
-function createActions() {
-  return {};
+type Actions<Shape> = Shape & {
+  addAction: <Key extends string, Action extends (...args: any[]) => any>(
+    key: Key,
+    action: Action
+  ) => Actions<Shape & { [Property in Key]: Action }>;
+};
+
+function createActions<Shape extends Record<string, any>>(
+  currentActions: Shape
+): Actions<Shape> {
+  return {
+    ...currentActions,
+    addAction: (key, action) => {
+      const newActions = { ...currentActions, [key]: action };
+      return createActions(newActions);
+    },
+  } as Actions<Shape>;
 }
 
-export default function createApp() {
-  const actions = createActions();
+let testApp = createActions({});
 
-  return {};
-}
+testApp = testApp.addAction('Test', () => {
+  console.log('Test');
+});
+
+testApp.Test();
