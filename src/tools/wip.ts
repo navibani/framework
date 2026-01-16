af04be6d2001e65589c1ed853d08b968fba0bd84
Navraj Bains <navraj.bains.89@outlook.com>
2026-01-16 00:16:48 -0500

    Added actions retainment function.

--- src/tools/wip.ts
+++ src/tools/wip.ts
@@ -1,3 +1,11 @@
-function createApp() {
+function createActions(): { [key: string]: (...args: any[]) => any } {
   return {};
+}
+
+function createApp() {
+  const actions = createActions();
+
+  return {
+    ...actions,
+  };
 }
