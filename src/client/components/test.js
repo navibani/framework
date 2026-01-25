07bd7ee6064cf7d90e1959b5adcc98ce9bf707ae
Navraj Bains <navraj.bains.89@outlook.com>
2026-01-25 16:15:51 -0500

    Extended component for shadow dom use.

--- src/client/components/test.js
+++ src/client/components/test.js
@@ -1,7 +1,12 @@
+const template = document.createElement('template');
+template.innerHTML = `<p>This is a test component: <slot></slot></p>`;
+
 class TestComponent extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `<p>This is a test component!</p>`;
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    shadow.appendChild(template.content.cloneNode(true));
   }
 }
 
